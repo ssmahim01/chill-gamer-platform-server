df6f6bb7b58e22b3048be767e116a4db96af319c
ssmahim01 <mahimzayn666@gmail.com>
2024-12-05 19:39:00 +0600

    Added json file of vercel and Modified .gitignore file with index file of js and successfully deployed server side to vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const reviewCollection = client.db("reviewsDB").collection("reviews");
     const watchListCollection = client.db("watchListsDB").collection("watchLists");
@@ -33,6 +33,11 @@ async function run() {
 
     app.get("/reviews", async(req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews/highest-rated", async(req, res) => {
+      const result = await reviewCollection.find({}).sort({rating: -1}).limit(6).toArray();
       res.send(result);
     });
 
@@ -63,8 +68,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
